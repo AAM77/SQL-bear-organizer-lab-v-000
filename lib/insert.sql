30e14b38db582c55d178a78a3da7466dce01bca0
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-28 15:49:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,18 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate", 2, "M", "brown", "happy", 1);
+VALUES 
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Rowdy", 4, "M", "white", "playful", 1);
+("Mr. Chocolate", 2, "M", "brown", "happy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Tabitha", 5, "F", "black", "strong-willed", 1);
+("Rowdy", 4, "M", "white", "playful", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Sergeant Brown", 10, "F", "white", "serious", 1);
+("Tabitha", 5, "F", "black", "strong-willed", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Melissa", 7, "F", "light-brown", "carefree", 1);
+("Sergeant Brown", 10, "F", "white", "serious", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Grinch", 1, "M", "dark-brown", "grumpy", 1);
+("Melissa", 7, "F", "light-brown","carefree", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Wendy", 1, "F", "dark-brown", "friendly", 1);
+("Grinch", 1, "M", "dark-brown", "grumpy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES (NULL, 1, "F", "black", "aggressive", 0);
+("Wendy", 1, "F", "dark-brown", "friendly", 1);
+
+(NULL, 1, "F", "black", "aggressive", 0);
